09147175d7c85a40ea9f2f5ab8cbf996e00cbe52
Gregory Lemercier <g.lemercier@samsung.com>
2017-11-10 20:42:08 +0100

    scripts: update update_version.sh to match new .pc format
    Change-Id: Iba120cd9bb206036b00809abcdae89289ad886fc
    Signed-off-by: Gregory Lemercier <g.lemercier@samsung.com>

--- scripts/update_version.sh
+++ scripts/update_version.sh
@@ -12,15 +12,15 @@ sed -i'' "s/LIB_VERSION_MAJOR [0-9]*/LIB_VERSION_MAJOR ${MAJOR}/" CMakeLists.txt
 sed -i'' "s/LIB_VERSION_MINOR [0-9]*/LIB_VERSION_MINOR ${MINOR}/" CMakeLists.txt
 sed -i'' "s/LIB_VERSION_MAJOR=[0-9]*/LIB_VERSION_MAJOR=${MAJOR}/" Makefile
 sed -i'' "s/LIB_VERSION_MINOR=[0-9]*/LIB_VERSION_MINOR=${MINOR}/" Makefile
-sed -i'' "s/version==*.*/version=${VERSION}/g" pkgconfig/libartik-sdk-base.pc
-sed -i'' "s/version==*.*/version=${VERSION}/g" pkgconfig/libartik-sdk-bluetooth.pc
-sed -i'' "s/version==*.*/version=${VERSION}/g" pkgconfig/libartik-sdk-connectivity.pc
-sed -i'' "s/version==*.*/version=${VERSION}/g" pkgconfig/libartik-sdk-media.pc
-sed -i'' "s/version==*.*/version=${VERSION}/g" pkgconfig/libartik-sdk-sensor.pc
-sed -i'' "s/version==*.*/version=${VERSION}/g" pkgconfig/libartik-sdk-systemio.pc
-sed -i'' "s/version==*.*/version=${VERSION}/g" pkgconfig/libartik-sdk-wifi.pc
-sed -i'' "s/version==*.*/version=${VERSION}/g" pkgconfig/libartik-sdk-zigbee.pc
-sed -i'' "s/version==*.*/version=${VERSION}/g" pkgconfig/libartik-sdk-lwm2m.pc
-sed -i'' "s/version==*.*/version=${VERSION}/g" pkgconfig/libartik-sdk-mqtt.pc
+sed -i'' "s/version==*.*/version=${VERSION}/g" pkgconfig/libartik-sdk-base.pc.cmake
+sed -i'' "s/version==*.*/version=${VERSION}/g" pkgconfig/libartik-sdk-bluetooth.pc.cmake
+sed -i'' "s/version==*.*/version=${VERSION}/g" pkgconfig/libartik-sdk-connectivity.pc.cmake
+sed -i'' "s/version==*.*/version=${VERSION}/g" pkgconfig/libartik-sdk-media.pc.cmake
+sed -i'' "s/version==*.*/version=${VERSION}/g" pkgconfig/libartik-sdk-sensor.pc.cmake
+sed -i'' "s/version==*.*/version=${VERSION}/g" pkgconfig/libartik-sdk-systemio.pc.cmake
+sed -i'' "s/version==*.*/version=${VERSION}/g" pkgconfig/libartik-sdk-wifi.pc.cmake
+sed -i'' "s/version==*.*/version=${VERSION}/g" pkgconfig/libartik-sdk-zigbee.pc.cmake
+sed -i'' "s/version==*.*/version=${VERSION}/g" pkgconfig/libartik-sdk-lwm2m.pc.cmake
+sed -i'' "s/version==*.*/version=${VERSION}/g" pkgconfig/libartik-sdk-mqtt.pc.cmake
 sed -i'' "s/Version:            [0-9]*.[0-9]*/Version:            ${VERSION}/" specs/libartik-sdk.spec
 sed -i'' "s/PROJECT_NUMBER         = [0-9]*.[0-9]*/PROJECT_NUMBER         = ${VERSION}/" doc/Doxyfile
